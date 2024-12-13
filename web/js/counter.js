d7486f489337955623da3ed4bcb5e985d1bdd2a6
Yue He <joyceheyue@outlook.com>
2024-12-13 09:10:43 -0800

    update counter

--- web/js/counter.js
+++ web/js/counter.js
@@ -1,22 +1,25 @@
 // counter.js
 window.addEventListener('DOMContentLoaded', (event) => {
-    getVisitCount();
-  });
-  
-  
-  const functionApi = 'https://joycefunc.azurewebsites.net/api/hello';
-  
-  const getVisitCount = () => {
-    fetch(functionApi)
+  getVisitCount();
+});
+
+const functionApi = 'https://joycefunc.azurewebsites.net/api/hello';
+
+const getVisitCount = () => {
+  fetch(functionApi)
+      .then(response => response.text()) // Get the raw response as text
       .then(response => {
-        return response.text()
+          count = parseInt(response); // Convert to integer
+          if (isNaN(count)) {
+              console.error("Invalid response from API:", response);
+              document.getElementById('counter').innerText = "Error";
+          } else {
+              console.log("Hello, you are visitor number " + count);
+              document.getElementById('counter').innerText = response;
+          }
       })
-      .then(response => {
-        const count = parseInt(response);
-        console.log("Hello, you are visitor number " + count);
-        document.getElementById('counter').innerText = count;
-      }).catch(function (error) {
-        console.log("Failed to fetch visitor count:", error);
+      .catch(error => {
+          console.error("Failed to fetch visitor count:", error);
+          document.getElementById('counter').innerText = "Error";
       });
-    return count;
-  }
+};
